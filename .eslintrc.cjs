2f7f9bdb0e2e13549f3c801b6311b84f07e312fa
Matthew Mercuri <mercurimatthew@gmail.com>
2023-03-25 12:32:51 -0400

    cleanup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,39 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  'env': {
+  env: {
     'browser': true,
     'es2021': true
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended'
   ],
-  'overrides': [
+  overrides: [
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  'plugins': [
+  plugins: [
     'react',
     '@typescript-eslint'
   ],
-  'rules': {
+  rules: {
     'linebreak-style': [
       'error',
       'unix'
     ],
-    'quotes': [
+    quotes: [
       'error',
       'single'
     ],
-    'semi': [
+    semi: [
       'error',
       'always'
     ],
-    'indent': ['error', 2],
+    indent: ['error', 2],
     'react/react-in-jsx-scope': 'off'
   }
 };

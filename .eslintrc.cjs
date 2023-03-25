bad782212d60b9a0c4d7b125080edfbb833b9a7e
Matthew Mercuri <mercurimatthew@gmail.com>
2023-03-25 12:20:41 -0400

    configured eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,40 +1,39 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended'
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'linebreak-style': [
+      'error',
+      'unix'
     ],
-    'overrides': [
+    'quotes': [
+      'error',
+      'single'
     ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint'
+    'semi': [
+      'error',
+      'always'
     ],
-    'rules': {
-        'indent': [
-            'error',
-            4
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ]
-    }
+    'indent': ['error', 2],
+    'react/react-in-jsx-scope': 'off'
+  }
 };
